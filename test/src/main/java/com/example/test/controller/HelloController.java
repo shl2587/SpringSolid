69237686bfc81cf5f53bd1f6f681886ab4c4f218
shl2587 <smsg2587@gmail.com>
2024-09-08 17:50:49 +0900

    인프런 SOLID 원칙
    스프링 연동 전

---
+++ test/src/main/java/com/example/test/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.test.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+}
