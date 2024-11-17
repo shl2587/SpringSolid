d476240422ed9c07d32af8bf8a4394817fd75be8
shl2587 <smsg2587@gmail.com>
2024-11-17 19:06:33 +0900

    싱글톤 패턴 적용 테스트 코드

--- src/test/java/com/hello/core/singleton/SingletoneTest.java
+++ src/test/java/com/hello/core/singleton/SingletoneTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletoneTest {
 
     @Test
@@ -23,6 +25,19 @@ public class SingletoneTest {
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적요한 객체 사용")
+    void singletonService() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isNotSameAs(singletonService2);
+    }
+
 }
