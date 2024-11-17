81bda3ab57481c472463d35c197cfbc3e979b401
shl2587 <smsg2587@gmail.com>
2024-11-17 19:51:16 +0900

    싱글톤 코드 주의사항 (공유 자원 관리)

--- src/test/java/com/hello/core/singleton/SingletoneTest.java
+++ src/test/java/com/hello/core/singleton/SingletoneTest.java
@@ -5,6 +5,8 @@ import com.hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -38,6 +40,19 @@ public class SingletoneTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isNotSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 }
---
+++ src/test/java/com/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package com.hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자 10000원 주문
+        statefulService1.order("UserA", 10000);
+        // ThreadㅠB: B사용자 20000원 주문
+        statefulService1.order("UserB", 20000);
+
+        // ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
