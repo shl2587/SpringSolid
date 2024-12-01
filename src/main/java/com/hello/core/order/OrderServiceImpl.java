e53fdee6102002b0538c4f8f3e7d716516b7593d
shl2587 <smsg2587@gmail.com>
2024-12-02 08:25:10 +0900

    생성자 주입 방식의 필요성

--- src/main/java/com/hello/core/order/OrderServiceImpl.java
+++ src/main/java/com/hello/core/order/OrderServiceImpl.java
@@ -11,15 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
 
     /**
@@ -29,6 +22,18 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
