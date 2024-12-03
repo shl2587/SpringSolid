ae9a264115e053c03827b00cdca0c0d83e13c227
shl2587 <smsg2587@gmail.com>
2024-12-03 20:22:07 +0900

    동적 빈 조회를 위한 수동 등록

---
+++ src/main/java/com/hello/core/DiscountPolicyConfig.java
@@ -0,0 +1,21 @@
+package com.hello.core;
+
+import com.hello.core.discount.DiscountPolicy;
+import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DiscountPolicyConfig {
+
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
