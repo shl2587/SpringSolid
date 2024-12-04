9a147923710ea7cce54d6264ccdbb480b46b1f92
DESKTOP-KE3OJFB\INTECH-PC-NEW <smsg2587@gmail.com>
2024-12-05 08:26:40 +0900

    InitializingBean, DisposableBean 코드 설명

--- src/test/java/com/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/com/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package com.hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -20,11 +23,26 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message : " + message);
+        System.out.println("call : " + url + " message : " + message);
     }
 
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    // 빈 등록 이후 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 생명 주기 종료 후 실행
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
